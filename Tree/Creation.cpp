3b04e5da606e683101c873427e7b850c9a33eb72
rahiaag <135974912+rahiaag@users.noreply.github.com>
2024-01-01 23:19:11 +0530

    Creation.cpp
    Creating the Node Using the constructor and function both

--- Tree/Creation.cpp
+++ Tree/Creation.cpp
@@ -7,6 +7,12 @@ public:
     int data;
     node *left;
     node *right;
+
+node (int x)    // Creating node using Constructor
+{
+    data=x;
+    left=NULL;
+    right=NULL;
 };
 
 node *createNode(int data)
@@ -139,6 +145,20 @@ int main()
     */
 
     // preorder traversal
+
+    // Constructing Node using Constructor
+    node *p = new node(2);
+    node *p1 = new node(1);
+    node *p2 = new node(4);
+    node *p3 = new node(5);
+    node *p4 = new node(7);
+    node *p5 = new node(6);
+    node *p6 = new node(3);
+    node *p7 = new node(9);
+    node *p8 = new node(8);
+
+
+    
 
     // Constructing the root node - Using Function (Recommended)
     node *p = createNode(2);
